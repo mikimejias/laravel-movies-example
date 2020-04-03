ded06bf883637e6261544f4681c9f3f711d272a2
DarthMK <miki.mejias85@gmail.com>
2020-04-02 20:50:14 -0400

    Se editó el input de Search para hacer la búsqueda de películas y mostrarlas en un Dropdown usando Livewire component

--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -5,17 +5,24 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ViewMovieTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    /* public function testBasicTest()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    } */
+
+    public function the_main_page_shows_correct_info()
+    {
+        $response = $this->get(route('movies.index'));
+
+        $response->assertSuccessful();
     }
 }
