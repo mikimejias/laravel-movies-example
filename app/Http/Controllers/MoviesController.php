517b9f9c55e86511369a74572cac46f88398bca5
DarthMK <miki.mejias85@gmail.com>
2020-04-09 18:01:52 -0400

    Refactorizada la vista principal en MoviesViewModel

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
+use App\ViewModels\MovieSViewModel;
+
 class MoviesController extends Controller
 {
     /**
@@ -22,25 +24,37 @@ class MoviesController extends Controller
             ->get('https://api.themoviedb.org/3/movie/now_playing')
             ->json()['results'];
         
-        $genresArray = Http::withToken(config('services.tmdb.token'))
+        /* $genresArray = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/genre/movie/list')
             ->json()['genres'];
 
         $genres = collect($genresArray)->mapWithKeys(function ($genre)
         {
             return [$genre['id'] => $genre['name']];
-        });
+        }); */
+
+        $genres = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/movie/list')
+            ->json()['genres'];
 
         //dump($popularMovies);
         //dump($genresArray);
         //dump($genres);
         //dump($nowPlayingMovies);
 
-        return view('index', [
+        /* return view('index', [
             'popularMovies' => $popularMovies,
             'nowPlayingMovies' => $nowPlayingMovies,
             'genres' => $genres,
-        ]);
+        ]); */
+
+        $viewModel = new MoviesViewModel (
+            $popularMovies,
+            $nowPlayingMovies,
+            $genres
+        );
+
+        return view('index', $viewModel);
     }
 
     /**
---
+++ app/ViewModels/MovieViewModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\ViewModels;
+
+use Spatie\ViewModels\ViewModel;
+
+class MovieViewModel extends ViewModel
+{
+    public $movie;
+
+    public function __construct($movie)
+    {
+        $this->movie = $movie;
+    }
+}
