6bded555d22b0a18e1ef6c3a7be7bd4dd8087cf3
DarthMK <miki.mejias85@gmail.com>
2020-04-09 15:52:19 -0400

    Mostrando las imágenes en modal

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -60,7 +60,7 @@
               style="background-color: rgba(0, 0, 0, .5);"
               class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
               x-show.transition.opacity="isOpen"
-            >
+              >
               <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
                 <div class="bg-gray-900 rounded">
                   <div class="flex justify-end pr-4 pt-2">
@@ -118,7 +118,7 @@
     </div>
     {{-- end movie-cast --}}
 
-    <div class="movie-images">
+    <div class="movie-images" x-data=" { isOpen: false, image: '' } ">
       <div class="container mx-auto px-4 py-16">
         <h2 class="text-4xl font-semibold">Images</h2>
 
@@ -128,7 +128,7 @@
             @if ($loop->index < 9)
 
               <div class="mt-8">
-                <a href="#">
+                <a @click.prevent="isOpen = true" href="#">
                   <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $image['file_path'] }}" alt="{{ $image['file_path'] }}" class="hover:opacity-75 transition ease-in-out duration-150">
                 </a>
               </div>
@@ -139,6 +139,28 @@
 
           
         </div>
+
+        <div
+          style="background-color: rgba(0, 0, 0, .5);"
+          class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+          x-show="isOpen"
+        >
+          <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+            <div class="bg-gray-900 rounded">
+              <div class="flex justify-end pr-4 pt-2">
+                <button
+                  @click="isOpen = false"
+                  @keydown.escape.window="isOpen = false"
+                  class="text-3xl leading-none hover:text-gray-300">&times;
+                </button>
+              </div>
+              <div class="modal-body px-8 py-8">
+                <img :src="image" alt="poster">
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
     {{-- end movie-images --}}
